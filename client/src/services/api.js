cbd0d8431261f227f214a27f500ee6c6e05f7cfe
Sivani <sivanijyotsna@gmail.com>
2026-05-27 23:51:21 +0530

    fixed api routes correctly

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -3,9 +3,7 @@ import axios from "axios";
 const API = axios.create({
 
 baseURL:
-window.location.hostname === "localhost"
-? "http://localhost:5000/api"
-: "https://ecommerce-project-1-g5vy.onrender.com/api"
+"https://ecommerce-project-1-g5vy.onrender.com/api"
 
 });
 
