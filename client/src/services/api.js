b8413b7ed981b5dd92d8c1aec99abb4f4c01296d
Sivani <sivanijyotsna@gmail.com>
2026-05-28 00:25:09 +0530

    fixed products issue

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://ecommerce-project-1-g5vy.onrender.com",
+  baseURL: "https://ecommerce-project-1-g5vy.onrender.com"
 });
 
 export default API;
