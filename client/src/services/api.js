2b8ebd724db4576892e0ea72ba1fdfb0fadc57a8
Sivani <sivanijyotsna@gmail.com>
2026-05-27 23:42:10 +0530

    fixed api url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const API=axios.create({
+const API = axios.create({
 
 baseURL:
-"http://localhost:5000/api"
+"https://ecommerce-project-1-g5vy.onrender.com/api"
 
 });
 
