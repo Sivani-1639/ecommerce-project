7507692ca4a689190321a8c4ac8fb43497c98bb5
Sivani <sivanijyotsna@gmail.com>
2026-05-28 10:49:27 +0530

    final ecommerce fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/cart" element={<Cart />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+
+      <Route path="/cart" element={<Cart />} />
+    </Routes>
   );
 }
 
